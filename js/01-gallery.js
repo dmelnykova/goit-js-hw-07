363a5110b5f279b9f06a8ed2430e2e9af7c1503b
dariameln1 <dariamelnykova1@gmail.com>
2023-09-23 18:43:31 +0200

    start task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector('.gallery');
+const markup = createMarkup(galleryItems);
 
-console.log(galleryItems);
+container.insertAdjacentHTML('beforeend', markup);
+container.addEventListener('click', handlerItemClick);
+
+
+
+
+function createMarkup(arr) {
+    return arr.map((id, preview, original, description) => {
+        return `<li data-id="${id}" class="gallery__item gallery__image js-list-item">
+        <img src="${original}" alt="${description}"/>
+        </li>`
+    }).join('');    
+}
+
+function handlerItemClick(event) {
+    console.log(event.target);
+    
+}
+
+// console.log(galleryItems);
