07abe34e4e1ab35590c80bb0d741824c5197e883
dariameln1 <dariamelnykova1@gmail.com>
2023-09-25 17:09:18 +0200

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+
+list.insertAdjacentHTML('beforeend', markup);
+
+function createMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}"/>
+            </a>
+        </li>`;
+    }).join('');  
+}
+    
+const lightboxModal = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    optionsPosition: 'bottom',
+    captionDelay: 250,
+});
+
+
+// list.addEventListener('click', handleClick);
+
+// function handleClick(event) {
+//     event.preventDefault();
+//     if (event.target.nodeName !== "IMG") {
+//         return;
+//     }
+//     const instance = basicLightbox.create(`
+//     <img src="${event.target.dataset.source}" alt="${event.target.alt}"/>
+//     `, {
+//         onShow: () => { document.addEventListener("keydown", onEscPress) },
+//         onClose: () => { document.removeEventListener("keydown", onEscPress) },
+//     });
+//     instance.show();
+
+//     function onEscPress(event) {
+//         if (event.code === "Escape") {
+//             instance.close();
+//         }
+//     };
+// }
